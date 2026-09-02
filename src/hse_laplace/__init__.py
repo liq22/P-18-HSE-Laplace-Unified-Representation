17c36baab0be72224103a0f2ee9be61da1e693fe
liq22 <142728467+liq22@users.noreply.github.com>
2026-09-02 10:52:40 +0800

    refactor: export four-block representation API

--- src/hse_laplace/__init__.py
+++ src/hse_laplace/__init__.py
@@ -1,22 +1,24 @@
-"""Minimal analytic components for HSE-Laplace unified representation."""
+"""Analytic components for HSE-Laplace unified representation."""
 
 from .modal import modal_block, modal_transition, stable_pole_chart
 from .observability import (
     ObservableDecomposition,
     observable_projector,
-    shared_private_unobserved,
+    shared_private_missing_null,
+    soft_modal_observability,
 )
 from .representation import UnifiedRepresentation
-from .transport import compensated_gaussian_drift, block_euler_step
+from .transport import block_update_witness, compensated_gaussian_drift
 
 __all__ = [
     "ObservableDecomposition",
     "UnifiedRepresentation",
-    "block_euler_step",
+    "block_update_witness",
     "compensated_gaussian_drift",
     "modal_block",
     "modal_transition",
     "observable_projector",
-    "shared_private_unobserved",
+    "shared_private_missing_null",
+    "soft_modal_observability",
     "stable_pole_chart",
 ]
